e2f6c6717511dd54bfc5a4516068fb28acade7e0
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2022-09-23 10:00:48 -0300

    Curso finalizado

--- MinhaApp/Startup.cs
+++ MinhaApp/Startup.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MinhaApp
 {
@@ -46,6 +41,8 @@ namespace MinhaApp
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseMeuMiddleware();
 
             app.UseEndpoints(endpoints =>
             {
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # curso_desenvolvedor_iniciando_asp.net_core
 Curso Iniciando com ASP.NET Core
+
+# Desenvolvedor.io
+Eduardo Pires 
